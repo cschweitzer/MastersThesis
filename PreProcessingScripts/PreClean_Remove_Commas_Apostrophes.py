cf0b71a128787eab043e6e86fe517fabcc6dc9c4
Katie Schweitzer <u191699@titanium.uvt.nl>
2018-12-06 13:30:04 +0100

    Added utf8 encoding

--- PreProcessingScripts/PreClean_Remove_Commas_Apostrophes.py
+++ PreProcessingScripts/PreClean_Remove_Commas_Apostrophes.py
@@ -1,7 +1,7 @@
 
 #%%
-file = open('../../BigData/core_appevents.csv', 'r')
-output = open('../../Outputs/PreProcessing/core_appevents_stripped.csv', 'w')
+file = open('../../BigData/core_appevents.csv', 'r', encoding = 'utf-8')
+output = open('../../Outputs/PreProcessing/core_appevents_stripped.csv', 'w',encoding = 'utf-8')
 #%%
 
 for line in file:
